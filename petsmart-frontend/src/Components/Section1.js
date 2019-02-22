04070f1c99baf33c93c71b0d24d99238c04737d3
Michael Rojas <mikerojaswa@gmail.com>
2019-02-21 22:00:37 -0700

    Section 1 input done

--- petsmart-frontend/src/Components/Section1.js
+++ petsmart-frontend/src/Components/Section1.js
@@ -3,38 +3,50 @@ import Form from 'react-bootstrap/Form'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 const Section1 = () => (
-    <div>
-    <Form>
-        <h2>
-    Information 
-  </h2>
-  <Form.Group controlId="formEmployeeName">
-    <Form.Label>Employee Name</Form.Label>
-    <Form.Control type="text" placeholder="Enter name" />
-  </Form.Group>
+  <div>
+    <h2>Information</h2>
+    <Form.Group controlId="formEmployeeName">
+      <Form.Label>Employee Name</Form.Label>
+      <Form.Control type="text" placeholder="Enter name" />
+    </Form.Group>
 
-<Form.Group controlId="formEmployeeId">
-    <Form.Label>Employee Id</Form.Label>
-    <Form.Control type="text" placeholder="123456" readonly="readonly" />
-  </Form.Group>
-<h2>
-    Address 
-  </h2>
-  <Form.Group controlId="formAddress1">
-    <Form.Control type="text" placeholder="Address Line 1" />
-  </Form.Group>
-  <Form.Group controlId="formAddress2">
-    <Form.Control type="text" placeholder="Address Line 2" />
-  </Form.Group>
-  
-  <select>
-  <option value="USA">USA</option>
-  <option value="CA">Canada</option>
-</select>
+    <Form.Group controlId="formEmployeeId">
+      <Form.Label>Employee Id</Form.Label>
+      <Form.Control type="text" placeholder="123456" readonly="readonly" />
+    </Form.Group>
+    <h2>Address</h2>
+    <Form.Group controlId="formAddress1">
+      <Form.Control type="text" placeholder="Address Line 1" />
+    </Form.Group>
 
-  </Form>
-</div>
-)
+    <Form.Group controlId="formAddress2">
+      <Form.Control type="text" placeholder="Address Line 2" />
+    </Form.Group>
 
+    <Form.Group controlId="formCity">
+      <Form.Control type="text" placeholder="City" />
+    </Form.Group>
+
+    <select>
+      <option value="USA">USA</option>
+      <option value="CA">Canada</option>
+    </select>
+
+    <select>
+      <option value="State / Province">State / Province</option>
+    </select>
+    <Form.Group controlId="forZipCode">
+      <Form.Control type="text" placeholder="ZipCode" />
+    </Form.Group>
+
+    <h2>Reason for Reimbursement Request</h2>
+    <select>
+      <option value="Reason">Reason</option>
+    </select>
+    <Form.Group controlId="formReasona">
+      <Form.Control type="text" placeholder="Reason Details" />
+    </Form.Group>
+  </div>
+);
 
 export default Section1
