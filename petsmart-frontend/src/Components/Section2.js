b8656cee655f61b028112c3491c9328e1f311df3
Hadley Griffin <hadleymgriffin@gmail.com>
2019-02-21 23:18:43 -0700

    table on page 2

--- petsmart-frontend/src/Components/Section2.js
+++ petsmart-frontend/src/Components/Section2.js
@@ -1,8 +1,79 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const Section2 = () => (
-<div>
-</div>
-)
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
-export default Section2
+let id = 0;
+function createData(label, monday, tuesday, wednesay, thursday, friday, saturday, sunday) {
+  id += 1;
+  return { id, label, monday, tuesday, wednesay, thursday, friday, saturday, sunday};
+}
+
+const rows = [
+  createData('List Travel Dates:'),
+  createData('Store or city/state travelled from/to:'),
+  createData('Breakfast'),
+  createData('Lunch'),
+  createData('Dinner'),
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="right">Monday</TableCell>
+            <TableCell align="right">Tuesday</TableCell>
+            <TableCell align="right">Wednesday</TableCell>
+            <TableCell align="right">Thursday</TableCell>
+            <TableCell align="right">Friday</TableCell>
+            <TableCell align="right">Saturday</TableCell>
+            <TableCell align="right">Sunday</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.monday}</TableCell>
+              <TableCell align="right">{row.tuesday}</TableCell>
+              <TableCell align="right">{row.wednesday}</TableCell>
+              <TableCell align="right">{row.thursday}</TableCell>
+              <TableCell align="right">{row.friday}</TableCell>
+              <TableCell align="right">{row.saturday}</TableCell>
+              <TableCell align="right">{row.sunday}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
